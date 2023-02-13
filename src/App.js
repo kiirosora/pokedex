875b070c41bcdad9f2b9a951591f48cf60c26008
kiirosora <kiirosora.code@gmail.com>
2023-02-13 12:49:17 +0800

    Added: SearchBar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import { useState, useEffect } from "react";
 import DisplayPokeList from "./DisplayPokeList";
 import DisplayPokeInfo from "./DisplayPokeInfo";
 import ImportPokeInfo from "./ImportPokeInfo";
-import { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
 import "./App.scss";
 
 function App() {
   const [showInfo, setShowInfo] = useState(false);
   const [showInfoID, setShowInfoID] = useState(0);
   const [pokemons, setPokemons] = useState([]);
-  const [filters, setFilters] = useState([])
+  const [filters, setFilters] = useState({})
   const [isImportingData, setIsImportingData] = useState(false);
 
   // Fetch initial data on mount
   useEffect(() => {
     console.log("App.js Fetch");
-    fetch("http://localhost:3000/pokemons")
+    fetch("http://localhost:3000/pokemons?_sort=id")
       .then((response) => response.json())
       .then((data) => {
-        data.sort((a, b) => {
+        /* data.sort((a, b) => {
           return a.id - b.id;
-        });
+        }); */
         // setIsImportingData(true);
         setPokemons(data);
       });
   }, []);
+
+console.log(filters);
 
   return (
     <div className="App">
@@ -35,8 +38,10 @@ function App() {
         />
       )}
       <h1 className="text-center mt-5 mb-5">Pokedex</h1>
+      <SearchBar setFilters={setFilters}/>
       <DisplayPokeList
         pokemons={pokemons}
+        filters={filters}
         setShowInfo={setShowInfo}
         setShowInfoID={setShowInfoID}
       />
